ec5370971f9acae197fa8aadecb4a39a4f3ebdd4
maj-dan <160074910+maj-dan@users.noreply.github.com>
2024-07-16 20:41:09 -0300

    Switch tab with nav btns

--- src/index.js
+++ src/index.js
@@ -3,4 +3,30 @@ import menuPage from "./menu.js";
 import aboutPage from "./about.js";
 import "./style.css";
 
-document.querySelector("#content").appendChild(aboutPage());
+document.querySelector("#content").appendChild(homePage());
+
+(function(){
+    document.querySelector("nav").addEventListener("click", switchTab);
+
+    function switchTab(event) {
+        //ckecking if it's a valid btn, to use one event listener with event bubbling
+        if(!(/-btn$/.test(event.target.id))) return;
+    
+        document.querySelector("#content > div").remove();
+    
+        switch (event.target.id) {
+            case "menu-btn":
+                renderPage(menuPage());
+                break;
+            case "home-btn":
+                renderPage(homePage());
+                break;
+            case "about-btn":
+                renderPage(aboutPage());
+        }
+    }
+    
+    function renderPage(page) {
+        document.querySelector("#content").appendChild(page);
+    }
+})();
